dacb3f1167e9a29e2c527670858f8ebad35bbc96
harsha <harsha181403@gmail.com>
2026-05-02 03:04:25 +0530

    feat: separate View Details and Track Live into dedicated UI experiences

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import ReportDetail from "./pages/ReportDetail";
+import TrackLive from "./pages/TrackLive";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -18,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/report/:id" element={<ReportDetail />} />
-          <Route path="/track/:id" element={<ReportDetail />} />
+          <Route path="/track/:id" element={<TrackLive />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
